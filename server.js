addeed137196c7a71108a8bd74e08de0e275d647
Vu Ky <vukynpro@gmail.com>
2022-04-16 00:41:17 +0700

    redirect with shorten url

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const dns = require('dns');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -22,25 +23,51 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-const ShortUrl = require("./shorturl.js").ShortUrlModel;
-const createShortUrl = require("./shorturl.js").createAndSave;
+const ShortUrl = require('./shorturl.js').ShortUrlModel;
+const createShortUrl = require('./shorturl.js').createAndSave;
+const findOriginalUrl = require('./shorturl.js').findOriginalByShortUrl;
 
+app.post("/api/shorturl", (req, res, next) => {
 
-app.post("/api/shorturl", (req, res) => {
-  createShortUrl(req.body.url, (err, data) => {
-    if (err) {
-      return next(err);
+  // Check invalid url format
+  const HTTPS_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+  if (!(req.body.url.match(new RegExp(HTTPS_REGEX))))
+    return res.json({ error: "Invalid URL" });
+
+  //Check invalid hostname
+  const REPLACE_REGEX = /^https?:\/\//i
+  const _url = req.body.url.replace(REPLACE_REGEX, '');
+  dns.lookup(_url, { family: 6, hints: dns.ADDRCONFIG | dns.V4MAPPED }, (err) => {
+    if (err)
+      res.json({ error: "Invalid Hostname" });
+    else {
+      createShortUrl(req.body.url, (err, data) => {
+        if (err) {
+          return next(err);
+        }
+        ShortUrl.findById(data._id, function (err, url) {
+          if (err) {
+            return next(err);
+          }
+          res.json({ original_url: url.original_url, short_url: url.short_url });
+        });
+      });
     }
-    ShortUrl.findById(data._id, function (err, pers) {
-      if (err) {
-        return next(err);
-      }
-      res.json(pers);
-      //pers.remove();
-    });
   });
 });
 
+app.get("/api/shorturl/:short_url", (req, res) => {
+  findOriginalUrl(req.params.short_url, (err, url) => {
+    if (err) {
+      return next(err);
+    }
+    if (!url) {
+      res.json({ error: "No short URL found for the given input" });
+    }
+    else
+      res.redirect(url.original_url);
+  });
+});
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
--- shorturl.js
+++ shorturl.js
@@ -1,35 +1,31 @@
 require('dotenv').config();
 var mongoose = require('mongoose');
+var randomstring = require('randomstring');
 const { Schema } = mongoose;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const shorturlSchema = new Schema({
     original_url: { type: String, required: true },
-    short_url: { type: Number, default: 0 }
+    short_url: { type: String, required: false }
 });
 
 var ShortUrl = mongoose.model("shorturl", shorturlSchema);
 
-// var entitySchema = mongoose.Schema({
-//     testvalue: { type: String }
-// });
-
-// entitySchema.pre('save', function (next) {
-//     var doc = this;
-//     shorturl.findByIdAndUpdate({ _id: 'entityId' }, { $inc: { short_url: 1 } }, function (error, counter) {
-//         if (error)
-//             return next(error);
-//         doc.testvalue = counter.seq;
-//         next();
-//     });
-// });
-
+const findOriginalByShortUrl = (short_url, done) => {
+    ShortUrl.findOne({ short_url: short_url }, (err, data) => {
+        if (err) return console.log(err);
+        done(null, data);
+    });
+};
 
 const createAndSave = (original_url, done) => {
+
     const _shorturl = new ShortUrl({
-        original_url: original_url
+        original_url: original_url,
+        short_url: randomstring.generate(7)
     });
+
     _shorturl.save(function (err, data) {
         if (err) return console.error(err);
         done(null, data);
@@ -38,3 +34,4 @@ const createAndSave = (original_url, done) => {
 
 exports.ShortUrlModel = ShortUrl;
 exports.createAndSave = createAndSave;
+exports.findOriginalByShortUrl = findOriginalByShortUrl;
